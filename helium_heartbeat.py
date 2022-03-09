c3706aa3ec17b6943257afe1c6b768d8c593c9c4
Sam Gutentag <developer@samgutentag.com>
2022-03-09 14:41:16 -0800

    stripped down and added comments

--- helium_heartbeat.py
+++ helium_heartbeat.py
@@ -5,27 +5,19 @@
 Track the sync status, most recent block activity, ping, and other
 metrics for each hotspot.
 
-Inspiration from https://gutentag.co/google_python_docstrings
-and https://gutentag.co/google_python_styleguide
-
-Use CalVer versioning as described at https://calver.org/
-
 """
 
 __authors__ = ["Sam Gutentag"]
 __email__ = "developer@samgutentag.com"
 __maintainer__ = "Sam Gutentag"
-__version__ = "2022.03.09.0"
+__version__ = "2022.03.09.1"
 
 
 import json
 import logging
 import os
-import statistics
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-
-from tcp_latency import measure_latency
 
 import helium_api_wrapper as pythelium
 
@@ -56,49 +48,26 @@ def setup_logging():
     logging.debug("-" * 80)
 
 
-def get_most_recent_activity(hotspot=None):
-    """Get most recent activity block height."""
+def get_latest_active_block(hotspot=None):
+    """Get most recent activity block height.
+
+    Active is defined as "activity recorded to the Helium Blockchain"
+
+    Args:
+        hotspot (dict): a hotspot information dictionary
+
+    Returns:
+        result (int): block height hotspot was most recently active
+
+    Raises:
+        Exception: if anything goes wrong, return -1
+
+    """
     try:
         _activity = list(pythelium.hotspot_activity(hotspot["address"], max_depth=3))[0]
         return _activity["height"]
     except Exception:
         return -1
-
-
-def get_ping_time(ip4_addr=None, port=44158):
-    """Ping specific port on IP address.
-
-    Returns dict of ping time average, median, and stdev in milliseconds
-
-    Args:
-        ip4_addr (string): public ip address to ping
-        port (int): port to check connection to
-
-    Returns:
-        result (dict): keys > [avg, median, stddev]
-
-    Raises:
-        Exception: if statistic can not be computed returns a -1
-
-    """
-    latency_result = measure_latency(host=ip4_addr, port=port, runs=5)
-
-    try:
-        _avg = statistics.mean(latency_result)
-    except Exception as e:
-        _avg = -1
-    try:
-        _median = statistics.median(latency_result)
-    except Exception as e:
-        _median = -1
-    try:
-        _stdev = statistics.stdev(latency_result)
-    except Exception as e:
-        _stdev = -1
-
-    result = {"avg": _avg, "median": _median, "stdev": _stdev}
-
-    return result
 
 
 def get_hotspot_heartbeat(hotspot=None):
@@ -121,22 +90,15 @@ def get_hotspot_heartbeat(hotspot=None):
     _heartbeat["name"] = _name
     _heartbeat["status_height"] = hotspot["status"]["height"]
     _heartbeat["status_timestamp"] = hotspot["status"]["timestamp"]
-
-    _listen_addrs = hotspot["status"]["listen_addrs"][0].split("/")[2]
-
-    _heartbeat["status_listen_addrs"] = _listen_addrs
-    _heartbeat["block"] = hotspot["block"]
-
-    # test hotspot ping
-    _heartbeat["ping_times"] = get_ping_time(ip4_addr=_listen_addrs)
+    _heartbeat["chain_height"] = hotspot["block"]
 
     # get latest activity block
-    _heartbeat["latest_activity_block"] = get_most_recent_activity(hotspot=hotspot)
+    _heartbeat["latest_activity_block"] = get_latest_active_block(hotspot=hotspot)
 
     return _heartbeat
 
 
-def get_heartbeats(wallet_addr=os.environ["WALLET_ADDR"]):
+def get_wallet_heartbeat(wallet_addr=os.environ["WALLET_ADDR"]):
     """Get heartbeat data for all hotspots in a given wallet.
 
     Multithreaded collection of heartbeat data for all hotspots associated to
@@ -175,7 +137,7 @@ def get_heartbeats(wallet_addr=os.environ["WALLET_ADDR"]):
     # get max block reported
     block_heights = []
     for name in sorted(heartbeats.keys()):
-        block_heights.append(heartbeats[name]["block"])
+        block_heights.append(heartbeats[name]["chain_height"])
     block_height_max = max(block_heights)
 
     # calculate inactive gap for each hotspot
@@ -190,55 +152,20 @@ def get_heartbeats(wallet_addr=os.environ["WALLET_ADDR"]):
     return heartbeats
 
 
-def update_info_files(wallet_heartbeats):
-    """update cached hotspot info files with latest heartbeat data
+def record_heartbeat_data(wallet_heartbeats=None):
+    """Write heartbeat data to json files.
 
-    Writes heartbeat data for wallet hotspots to a json file.
-    Only one copy is kept for each hotspot, updated with the most recent data
+    wallet heartbeat data is stored in JSON files for later processing
+    and analysis.
 
     Args:
-         wallet_heartbeats (dict): dictionary of hotspot heartbeat data
+         wallet_heartbeats (dict): wallet heartbeats data
+
+    Returns:
+         info_file (string): output filepath to recorded json data
 
     """
-    # ensure output directory exists
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    info_dir = os.path.join(this_dir, "data", "hotspot_latest_info")
-    if not os.path.isdir(info_dir):
-        os.makedirs(info_dir)
-
-    # for each hotspot in wallet_heartbeats, write data to json file
-    for hotspot, heartbeat in wallet_heartbeats.items():
-
-        hotspot_name = heartbeat["name"]
-        info_file = os.path.join(info_dir, f"{hotspot_name}-heartbeat.json")
-
-        # create info dictionary
-        hotspot_info = {}
-
-        # get info file if it exists
-        if os.path.exists(info_file):
-            ...
-            # open info file to dictionary
-            with open(info_file) as infile:
-                hotspot_info = json.load(infile)
-
-        # append hotspot heartbeat to key "heartbeat"
-        # overwrites existing entries
-        hotspot_info["heartbeat"] = heartbeat
-
-        # save data to info file
-        with open(info_file, "w", encoding="utf-8") as f:
-            json.dump(hotspot_info, f, ensure_ascii=False, indent=2)
-
-
-def main():
-    logging.debug("-" * 80)
-    logging.debug("Collecting Heartbeats for wallet.")
-    wallet_heartbeats = get_heartbeats()
-
-    # update hotspot info file with heartbeat data
-    update_info_files(wallet_heartbeats)
-
+    # get current utc time
     utcnow = datetime.utcnow()
     date_string = utcnow.strftime("%Y.%m.%d-%H:%M")
     date_file_str = utcnow.strftime("%Y.%m.%d-%H.%M")
@@ -248,22 +175,37 @@ def main():
     logging.debug(f"\tdata:\t{data}")
 
     # write heartbeat to json file
+    logging.debug("ensuring output directory exists...")
     this_dir = os.path.dirname(os.path.realpath(__file__))
     info_dir = os.path.join(
         this_dir,
         "data",
-        "heartbeat_info",
+        "wallet_heartbeats",
         f"{utcnow.strftime('%Y')}",
         f"{utcnow.strftime('%m')}",
     )
     if not os.path.isdir(info_dir):
         os.makedirs(info_dir)
+
+    # format info filepath
     info_file = os.path.join(info_dir, f"heartbeat-{date_file_str}.json")
 
     logging.debug(f"Writing heartbeat data to file: {info_file}")
     with open(info_file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
     logging.debug("\t...done")
+
+    return info_file
+
+
+def main():
+    logging.debug("-" * 80)
+    logging.debug("Collecting Heartbeats for wallet.")
+    wallet_heartbeats = get_wallet_heartbeat()
+
+    # write collected data to file.
+    logging.debug("storing wallet heartbeat data")
+    record_heartbeat_data(wallet_heartbeats=wallet_heartbeats)
 
 
 if __name__ == "__main__":
