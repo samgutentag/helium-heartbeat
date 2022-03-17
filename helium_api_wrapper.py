208a2ae646ada61d0e531b47538527a450398c7f
Sam Gutentag <developer@samgutentag.com>
2022-03-17 02:46:09 -0700

    remove global variable for helium api base url

--- helium_api_wrapper.py
+++ helium_api_wrapper.py
@@ -1,40 +1,30 @@
 #! /usr/bin/env python3
 
-"""Wrapper Functions for the Helium API
-
-
-Inspiration from https://gutentag.co/google_python_docstrings
-and https://gutentag.co/google_python_styleguide
-
-Use CalVer versioning as described at https://calver.org/
-
-"""
+"""Wrapper Functions for the Helium API."""
 
 __authors__ = ["Sam Gutentag"]
 __email__ = "developer@samgutentag.com"
 __maintainer__ = "Sam Gutentag"
-__version__ = "2022.03.09.1"
+__version__ = "2022.03.17.0"
 
 from os import getlogin
 from os.path import basename
 
 import requests
 
-API_HELIUM = "api.helium.io"
 HEADERS = {
     "User-Agent": f"solitaryPixels_{basename(__file__)}/{__version__}",
     "From": f"{getlogin()}.{basename(__file__)}@heliumheartbeat.com",
 }
 
 
-def hotspot_activity(address, cursor="", max_depth=2, api_url=API_HELIUM):
-    baseurl = f"https://{api_url}/v1/hotspots/{address}/roles"
+def hotspots_for_account(address, cursor="", max_depth=3, api_url="api.helium.io"):
+    baseurl = f"https://{api_url}/v1/accounts/{address}/hotspots"
     depth = 0
     while True and depth < max_depth:
         depth += 1
         _url = f"{baseurl}?cursor={cursor}"
         r = requests.get(_url, headers=HEADERS).json()
-
         try:
             for record in r["data"]:
                 yield record
